6eefcd687e4769669be2e18b5d283d01c5822c50
Ahsar-4rs <rashmeen18005@gmail.com>
2025-02-07 01:14:45 +0530

    explore

--- frontend/cams/src/App.js
+++ frontend/cams/src/App.js
@@ -1,11 +1,20 @@
 import Header from './Components/Header/Header.js'
 import './App.css';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Home/Homepage.js';
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-       
+      
+      
+       <BrowserRouter>
+       <Header/>
+        <Routes>
+            <Route exact path="/" element={<Homepage/>} />
+        </Routes>
+       </BrowserRouter>
     </div>
   );
 }
