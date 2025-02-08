cd42b09e4cb440a0a2d9990d8d67d1d63c7f6cfe
Ahsar-4rs <rashmeen18005@gmail.com>
2025-02-08 21:53:13 +0530

    upcoming events-homepage(3)

--- frontend/cams/src/pages/Home/Homepage.js
+++ frontend/cams/src/pages/Home/Homepage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Homepage.css'
 import Explore from '../../Components/Explore/Explore'
 import Section1 from '../../Components/Section1/Section1'
+import EventBanner from '../../Components/EventBanner/EventBanner'
 
 function Homepage() {
   return (
     <div>
       <Explore/>
       <Section1/>
+      <EventBanner/>
     </div>
   )
 }
