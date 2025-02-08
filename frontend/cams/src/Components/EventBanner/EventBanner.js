a5ddbe0993686f0a6da41c6bd0a7f298815a9283
Ahsar-4rs <rashmeen18005@gmail.com>
2025-02-08 21:53:13 +0530

    upcoming events-homepage(3)

--- frontend/cams/src/Components/EventBanner/EventBanner.js
+++ frontend/cams/src/Components/EventBanner/EventBanner.js
@@ -3,7 +3,9 @@ import './EventBanner.css'
 function EventBanner() {
   return (
     <div className='event-banner'>
-        
+        <div id='upcomingEvents'>
+            <h2 id='firstHalf'>Upcoming</h2><h2 id='secondHalf'> Events</h2>
+        </div>
     </div>
   )
 }
