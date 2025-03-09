94eeec7b86f3ea7ee8614e6b2b67e1940e5ff609
Nayana-S-Babu <nayandanasbabu@gmail.com>
2025-03-09 21:57:40 +0530

    Added new files and updates

---
+++ frontend/frontend/src/Components/Tabs/Tabs.jsx
@@ -0,0 +1,61 @@
+import './Tabs.css'
+import React from 'react'
+import health from './health-icon.png'
+import repair from './repair-icon.png'
+import emergency from './emergency-icon.png'
+import report from './report-icon.png'
+import event from './event-icon.png'
+
+
+function Tabs() {
+  
+  const tabs=[
+    {
+        name:"Health",
+        icon:health,
+        path:'/',
+    },
+    {
+        name:"Infrastructure",
+        icon:repair,
+        path:'/',
+    },
+    {
+        name:"Emergency Amenities",
+        icon:emergency,
+        path:'/',
+    },
+    {
+        name:"Social Events",
+        icon:event,
+        path:'/',
+    },
+    {
+        name:"Report",
+        icon:report,
+        path:'/',
+    }
+  ]
+  
+  
+  return (
+    <div className='tabs'>
+
+        
+        {tabs.map((item,i)=>{
+            
+            return(
+              <div className='category'>
+                <div className='content'>
+                    <img src={item.icon} alt=''/>
+                    <h3>{item.name}</h3>
+                </div> 
+              </div>
+              
+            )
+          })}
+    </div>
+  )
+}
+
+export default Tabs
