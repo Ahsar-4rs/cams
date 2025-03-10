19d1c89aedf224ae0091fbbb4a31b8794b5cab6f
Ahsar-4rs <rashmeen18005@gmail.com>
2025-03-10 07:14:53 +0530

    ect3

--- frontend/src/pages/Home/Homepage.jsx
+++ frontend/src/pages/Home/Homepage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Homepage.css'
-import Explore from '../Components/Explore/Explore.jsx'
-import Section1 from '../Components/Section1/Section1.jsx'
-import EventBanner from '../Components/Events/EventBanner.jsx'
+import Explore from '../../Components/Explore/Explore.jsx'
+import Section1 from '../../Components/Section1/Section1.jsx'
+import EventBanner from '../../Components/Events/EventBanner.jsx'
 
 
 function Homepage() {
