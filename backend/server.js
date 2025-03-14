019f3e541f851f0cf1cd921a93b2e9c3c707b849
Ahsar-4rs <rashmeen18005@gmail.com>
2025-03-14 20:22:36 +0530

    database,cors,express,cloudinary conneted to cam-cluster database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 import app from "./app.js"
+import cloudinary from "cloudinary"
+//install packages-npm install express mongoose jsonwebtoken cors express-fileupload bcrypt cloudinary cookie-parser validator dotenv
 
-app.listen(4000,()=>{
-    console.log(`Server listening on port ${4000}`)
+cloudinary.v2.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server listening on port ${process.env.PORT}`)
 });
+
