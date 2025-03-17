9f915553253eda5d5aca30fa0904f1aac77797fa
Afeef10703 <diosafeef@gmail.com>
2025-03-17 19:32:33 +0530

    Event Management partially completed

---
+++ frontend/src/pages/EventManagement/DeleteEvent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import placeholderimage from '../../assets/working.jpg';
+import './DeleteEvent.css';
+
+export default function DeleteEvent() {
+    return(
+        <div className="delete-event-container">
+            <img src={placeholderimage} alt="Work In Progress" />
+        </div>
+    )
+}
