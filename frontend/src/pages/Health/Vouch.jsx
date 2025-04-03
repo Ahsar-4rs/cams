af74619b72fb705bf792154bd3952cc8b8de39a9
Afeef10703 <diosafeef@gmail.com>
2025-04-03 11:22:53 +0530

    - Reports page added - Minor UI Improvements

---
+++ frontend/src/pages/Health/Vouch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Vouch() {
+    return (
+        <div>
+            <h1>Vouch</h1>
+            <p>This is the Vouch page.</p>
+        </div>
+    );
+}
+
+export default Vouch;
--- frontend/src/pages/Home/Homepage.jsx
+++ frontend/src/pages/Home/Homepage.jsx
@@ -3,13 +3,27 @@ import './Homepage.css'
 import Explore from '../../Components/Explore/Explore.jsx'
 import Section1 from '../../Components/Section1/Section1.jsx'
 import EventBanner from '../../Components/Events/EventBanner.jsx'
+import { useSession } from '../../context/SessionContext.jsx'
+import { useNavigate } from 'react-router-dom'
 
 
 function Homepage() {
+
+  const { getCurrentAccessRights } = useSession();
+  const rights = getCurrentAccessRights();
+  const navigate = useNavigate();
+  //console.log("User rights: ", rights);
+
+  const handleAdminControls = () => {
+    navigate('/admin-controls');
+  }
+
   return (
     <div>
       <Explore/>
       <Section1/>
+      {rights['canAlterSystem'] && (<button className='admin-button' onClick={handleAdminControls}>Admin Controls</button>
+      )}
       <EventBanner/>
     </div>
   )
