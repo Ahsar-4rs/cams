0f109ab8d798fe60d952c72e68c4d73308004bd2
Ahsar-4rs <rashmeen18005@gmail.com>
2025-04-05 20:14:12 +0530

    get some and get all verified with postman

--- backend/router/healthAlertRouter.js
+++ backend/router/healthAlertRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { reporthealthAlert } from "../controller/healthAlertController.js";
+import { getAllReports, getSomeReport, reporthealthAlert } from "../controller/healthAlertController.js";
 
 const router =express.Router();
 
 router.post("/reportAlert",reporthealthAlert);
+router.get("/getAllReport",getAllReports);
+router.get("/getSomeReport",getSomeReport);
 
 export default router;
