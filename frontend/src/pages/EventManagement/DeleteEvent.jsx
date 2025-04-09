327892b462318a58ad614dfea8c63406f1bc9e47
Nayana-S-Babu <nayandanasbabu@gmail.com>
2025-04-09 20:16:05 +0530

    added edit and delete action pages

--- frontend/src/pages/EventManagement/DeleteEvent.jsx
+++ frontend/src/pages/EventManagement/DeleteEvent.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import placeholderimage from '../../assets/working.jpg';
 import './DeleteEvent.css';
 
 export default function DeleteEvent() {
+    const { eventId } = useParams();
     return(
         <div className="delete-event-container">
             <img src={placeholderimage} alt="Work In Progress" />
